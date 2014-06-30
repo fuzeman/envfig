2cd8885a75377d94321a30f9b06e8b2496308d77
Dean Gardiner <gardiner91@gmail.com>
2014-06-30 17:13:32 +1200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='envfig',
+    version='1.0.0',
+    url='http://github.com/fuzeman/envfig/',
+
+    author='Dean Gardiner',
+    author_email='me@dgardiner.net',
+
+    description='',
+    packages=['envfig'],
+    platforms='any',
+
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python'
+    ],
+)
